f22c17837ea374f62653ab7658e1f67c0be8ab29
songbaochun <song18201303038@126.com>
2019-03-19 15:59:34 +0800

    注册界面搭建

--- app/src/main/java/com/example/coffee/testtimer/MainActivity.java
+++ app/src/main/java/com/example/coffee/testtimer/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Chronometer;
 import android.widget.Toast;
 
@@ -22,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.item_select_class);
+//        isLeaderDialog();
+    }
+
+    //班主任提示框ui界面
+    private void isLeaderDialog() {
+        AlertDialog IsHeadmasterDialog = new AlertDialog.Builder(this, R.style.common_dialog_style).create();
+        IsHeadmasterDialog.show();
+        Window window = IsHeadmasterDialog.getWindow();
+        window.setContentView(R.layout.dialog_class_info);
+        DisplayMetrics dm = this.getResources().getDisplayMetrics();
+        int displayWidth = dm.widthPixels;
+        int displayHeight = dm.heightPixels;
+        android.view.WindowManager.LayoutParams p = IsHeadmasterDialog.getWindow().getAttributes();
+        p.height = (int) (displayHeight * 0.6);    //宽度设置为屏幕的0.5
+//        dlg.setCanceledOnTouchOutside(false);// 设置点击屏幕Dialog不消失
+        window.setAttributes(p);     //设置生效
     }
 }
